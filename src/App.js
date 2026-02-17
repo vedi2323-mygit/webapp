ab87dfc01ec62f411be9070d35b0132c2d3f4f11
VEDI23 <vedims23@VediMS.localdomain>
2026-02-17 17:40:06 +0000

    initial load

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Happy maha shivratri!!! may the blessings of lord reach you and our wish comes true..May lord shiva bless you with grace and good healthy and wealthy life..Hope in yourself vediii
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
